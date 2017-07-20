3532bdc25046b7e48cee873707768921e63bca73
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-07-20 10:17:16 +0700

    add get by parent id

--- lib/indonesia/district.rb
+++ lib/indonesia/district.rb
@@ -1,8 +1,14 @@
 module Indonesia
-  def self.districts
+  def self.districts(regency_id = nil)
     @_districts ||= begin
-      data = CSV.open('lib/data/districts.csv')
+      data = CSV.read('lib/data/districts.csv')
       data.map { |r| { id: r[0].to_i, regency_id: r[1].to_i, name: r[2] } }
+    end
+
+    if regency_id.nil?
+      @_districts
+    else
+      @_districts.select { |d| d[:regency_id] == regency_id.to_i }
     end
   end
 end
--- lib/indonesia/village.rb
+++ lib/indonesia/village.rb
@@ -1,8 +1,14 @@
 module Indonesia
-  def self.villages
+  def self.villages(district_id = nil)
     @_villages ||= begin
-      data = CSV.open('lib/data/villages.csv')
+      data = CSV.read('lib/data/villages.csv')
       data.map { |r| { id: r[0].to_i, district_id: r[1].to_i, name: r[2] } }
+    end
+
+    if district_id.nil?
+      @_villages
+    else
+      @_villages.select { |v| v[:district_id] == district_id.to_i }
     end
   end
 end
